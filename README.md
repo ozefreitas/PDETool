c0dd20c62721fe48aabbec2c10b4ba424d40e527
pg42872 <pg42872@alunos.uminho.pt>
2022-03-06 16:12:45 +0000

    Reorganizing

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# PDETool
+# PDEFinder
+
+This is a simple example package. You can use
+[Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
+to write your content.
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="PDETool",
+    name="PDEFinder",
     version="1.0.0",
     author="José Freitas",
     author_email="jpsfreitas12@gmail.com",
@@ -12,10 +12,12 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pg42872/PDETool",
-    packages=setuptools.find_packages(),
-    classifiers=(
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ),
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
 )
