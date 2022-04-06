bc241944266a87c5947b178d9e5ed38391394c2b
Jose Freitas <pg42872@alunos.uminho.pt>
2022-04-06 23:47:15 +0100

    Change to functions

--- src/PDEFinder/Alignments/tsv_parser.py
+++ src/PDEFinder/Alignments/tsv_parser.py
@@ -1,35 +1,54 @@
 import pandas as pd
 
-# dar os nomes as colunas
-header = ["Query accession", "Target accession", "Sequence identity", 
-            "Length", "Mismatches", "Gap openings", "Query start", 
-            "Query end", "Target start", "Target end", "E-value", "Bit score"]
 
-# ler file em csv separado por tabs
-diamond_outfile = pd.read_csv("C:/Users/jpsfr/OneDrive/Ambiente de Trabalho/TOOL/PDETool/src/PDEFinder/Alignments/Diamond/diamond_out.tsv", sep="\t", names=header)
-print(diamond_outfile)
+def diamond_parser(filename):
+    """    Process a .tsv file from diamond tool output
 
-# selecionar colunas com perc. identity juntamente com os IDs das sequencias
-print(diamond_outfile.columns)
-seq_id = diamond_outfile[["Query accession", "Target accession", "Sequence identity"]]
-print(seq_id)
+    Args:
+        filename (string): name of the diamond output file in .tsv format
 
-# retirar os grupos de enzimas com similaridade de 60% a 90% com incrementos de 5%
-target_enzymes = {}
-for perc in range(60, 86, 5):
-    chave = str(perc)+"-"+str(perc+5)
-    for index, seq in seq_id.iterrows():
-        if seq["Sequence identity"] >= perc and seq["Sequence identity"] < perc+5:
-            if chave not in target_enzymes.keys():
-                target_enzymes[chave] = [seq["Target accession"]]
-            else:
-                target_enzymes[chave].append(seq["Target accession"])
+    Returns:
+        DataFrame: A pandas dataframe with diamond documented columns names as header
+    """
 
-print(target_enzymes.keys())
-print(target_enzymes.values())
+    # dar os nomes as colunas
+    header = ["Query accession", "Target accession", "Sequence identity", 
+                "Length", "Mismatches", "Gap openings", "Query start", 
+                "Query end", "Target start", "Target end", "E-value", "Bit score"]
 
-# passar a csv ambos 
-diamond_outfile.to_csv("C:/Users/jpsfr/OneDrive/Ambiente de Trabalho/TOOL/PDETool/src/PDEFinder/Alignments/Diamond/best_matches.csv")
-seq_id.to_csv("C:/Users/jpsfr/OneDrive/Ambiente de Trabalho/TOOL/PDETool/src/PDEFinder/Alignments/Diamond/sequences_identity.csv")
+    # ler file em csv separado por tabs
+    diamond_outfile = pd.read_csv("C:/Users/jpsfr/OneDrive/Ambiente de Trabalho/TOOL/PDETool/src/PDEFinder/Alignments/Diamond/diamond_out.tsv", sep="\t", names=header)
+    return diamond_outfile
 
-# PASSAR PARA FUNÇÃO E CRIAR NOVO SCRIPT PARA RETIRAR AS SEQUENCIAS DA UNIPROT
+# passar a csv
+# diamond_outfile.to_csv("C:/Users/jpsfr/OneDrive/Ambiente de Trabalho/TOOL/PDETool/src/PDEFinder/Alignments/Diamond/best_matches.csv")
+
+def iter_per_sim(dataframe):
+    """    Given a pandas DataFrame, return a dictionary with a list of sequences form the iteration of the sequence similarity between queries and database sequences.
+
+    Args:
+        dataframe (DataFrame): A pandas dataframe with diamond documented columns names as header
+
+    Returns:
+        dictionary: A dictionary where the keys are intervals of sequence similarity, and values are lists of UniProtKB ID's
+    """
+
+    # selecionar colunas com perc. identity juntamente com os IDs das sequencias
+    print(dataframe.columns)
+    seq_id = dataframe[["Query accession", "Target accession", "Sequence identity"]]
+    print(seq_id)
+
+    # retirar os grupos de enzimas com similaridade de 60% a 90% com incrementos de 5%
+    target_enzymes = {}
+    for perc in range(60, 86, 5):
+        chave = str(perc)+"-"+str(perc+5)
+        for index, seq in seq_id.iterrows():
+            if seq["Sequence identity"] >= perc and seq["Sequence identity"] < perc+5:
+                if chave not in target_enzymes.keys():
+                    target_enzymes[chave] = [seq["Target accession"]]
+                else:
+                    target_enzymes[chave].append(seq["Target accession"])
+    return target_enzymes
+
+# passar a csv
+# seq_id.to_csv("C:/Users/jpsfr/OneDrive/Ambiente de Trabalho/TOOL/PDETool/src/PDEFinder/Alignments/Diamond/sequences_identity.csv")
