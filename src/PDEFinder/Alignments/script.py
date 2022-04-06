519dd5938cca33e831ceebbc85a4461a8199b250
Jose Freitas <pg42872@alunos.uminho.pt>
2022-04-07 00:27:04 +0100

    Add script for testing

---
+++ src/PDEFinder/Alignments/script.py
@@ -0,0 +1,9 @@
+from tsv_parser import diamond_parser, iter_per_sim
+from uniprot_retriever import fasta_retriever
+
+df = diamond_parser("C:/Users/jpsfr/OneDrive/Ambiente de Trabalho/TOOL/PDETool/src/PDEFinder/Alignments/Diamond/diamond_out.tsv")
+
+enzymes = iter_per_sim(df)
+print(enzymes)
+
+fasta_retriever(enzymes, "test.fasta")
