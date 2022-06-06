0e993708f6ed5167503ddd0c0f99b26563dcc08f
Jose Freitas <pg42872@alunos.uminho.pt>
2022-06-06 14:51:00 +0100

    Updated

--- .tests/snakefile_test.py
+++ .tests/snakefile_test.py
@@ -1,14 +1,17 @@
-from workflow.PDEFinder.scripts.UPIMAPI_parser import UPIMAPI_parser, UPIMAPI_iter_per_sim, save_as_tsv
-from workflow.PDEFinder.scripts.seq_download import get_fasta_sequences
+from workflow.scripts.UPIMAPI_parser import UPIMAPI_parser, UPIMAPI_iter_per_sim, save_as_tsv
+from workflow.scripts.seq_download import get_fasta_sequences
 
 
-upi = UPIMAPI_parser("C:/Users/jpsfr/OneDrive/Ambiente de Trabalho/TOOL/PDETool/workflow/PDEFinder/Alignments/BLAST/results/upimapi_results/UPIMAPI_results.tsv")
-print(upi)
+# upi = UPIMAPI_parser("C:/Users/jpsfr/OneDrive/Ambiente de Trabalho/TOOL/PDETool/workflow/PDEFinder/Alignments/BLAST/results/upimapi_results/UPIMAPI_results.tsv")
+# print(upi)
+# 
+# upi_enzymes = UPIMAPI_iter_per_sim(upi)
+# print(upi_enzymes.keys())
+# # print(upi_enzymes)
+# save_as_tsv(upi_enzymes)
+# 
+# get_fasta_sequences("C:/Users/jpsfr/OneDrive/Ambiente de Trabalho/TOOL/PDETool/workflow/PDEFinder/Data/Tables/UPIMAPI_results_per_sim.tsv")~
 
-upi_enzymes = UPIMAPI_iter_per_sim(upi)
-print(upi_enzymes.keys())
-# print(upi_enzymes)
-save_as_tsv(upi_enzymes)
 
-get_fasta_sequences("C:/Users/jpsfr/OneDrive/Ambiente de Trabalho/TOOL/PDETool/workflow/PDEFinder/Data/Tables/UPIMAPI_results_per_sim.tsv")
+
 
